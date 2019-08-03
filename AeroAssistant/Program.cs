a4550bb998c157c5a6a9d4d0f7d9f905353201e8
Shravan Jambukesan <shravan.j97@gmail.com>
2019-08-03 16:33:25 -0500

    Improved UI and error handling

--- AeroAssistant/Program.cs
+++ AeroAssistant/Program.cs
@@ -19,18 +19,19 @@ namespace AeroAssistant
                 AerospikeClient client = new AerospikeClient(ip, port);
                 if (client.Connected)
                 {
-                    Console.WriteLine("Connected to aerospike cluster on host: " + ip);
+                    Console.WriteLine("Connected to Aerospike cluster on host: " + ip + ":" + port);
                     LaunchAssistant(client);
                 }
                 else
                 {
-                    Console.WriteLine("Could not connect to aerospike cluster on host: " + ip);
+                    Console.WriteLine("Could not connect to Aerospike cluster on host: " + ip + ":" + port);
                     Console.ReadKey();
                 }
             }
             catch(Exception e)
             {
                 Console.WriteLine("Could not connect to host");
+                Console.WriteLine("The following error occured: " + e);
                 Console.ReadKey();
             }
 
@@ -38,7 +39,7 @@ namespace AeroAssistant
 
         public static void LaunchAssistant(AerospikeClient client)
         {
-            Console.WriteLine("****AeroAssistant Main Menu****");
+            Console.WriteLine("**** AeroAssistant Main Menu ****");
             Console.WriteLine("1) Read records");
             Console.WriteLine("2) Write record");
             Console.WriteLine("3) Exit");
@@ -61,46 +62,69 @@ namespace AeroAssistant
 
         public static void ReadRecords(AerospikeClient client)
         {
-            Console.Write("Enter namespace: ");
-            string ns = Console.ReadLine();
-            Console.Write("Enter set name: ");
-            string setName = Console.ReadLine();
-            Console.Write("Enter key name: ");
-            string keyName = Console.ReadLine();
-            Key key = new Key(ns, setName, keyName);
-            Record record = client.Get(null, key);
-            if(record != null)
+            try
             {
-                foreach (KeyValuePair<string, object> entry in record.bins)
+                Console.Write("Enter namespace: ");
+                string ns = Console.ReadLine();
+                Console.Write("Enter set name: ");
+                string setName = Console.ReadLine();
+                Console.Write("Enter key: ");
+                string keyName = Console.ReadLine();
+                Key key = new Key(ns, setName, keyName);
+                Record record = client.Get(null, key);
+                Console.WriteLine("Query Result");
+                Console.WriteLine("======================================");
+                if(record != null)
                 {
-                    Console.WriteLine("Name: " + entry.Key + " Value: " + entry.Value);
+                    foreach (KeyValuePair<string, object> entry in record.bins)
+                    {
+                        Console.WriteLine("Name: " + entry.Key + " Value: " + entry.Value);
+                    }
                 }
+                Console.WriteLine("======================================");
+                LaunchAssistant(client);
             }
-            LaunchAssistant(client);
+            catch (Exception e)
+            {
+                Console.WriteLine("The following error occured while trying to read a record: ");
+                Console.WriteLine(e);
+                LaunchAssistant(client);
+            }
+
         }
 
         public static void WriteRecord(AerospikeClient client)
         {
-            WritePolicy policy = new WritePolicy();
-            policy.SetTimeout(50);
+            try
+            {
+                WritePolicy policy = new WritePolicy();
+                policy.SetTimeout(50);
 
-            Console.Write("Enter namespace: ");
-            string ns = Console.ReadLine();
-            Console.Write("Enter set name: ");
-            string setName = Console.ReadLine();
-            Console.Write("Enter key name: ");
-            string keyName = Console.ReadLine();
-            Console.Write("Enter bin name: ");
-            string binName = Console.ReadLine();
-            Console.Write("Enter value: ");
-            string value = Console.ReadLine();
+                Console.Write("Enter namespace: ");
+                string ns = Console.ReadLine();
+                Console.Write("Enter set name: ");
+                string setName = Console.ReadLine();
+                Console.Write("Enter key: ");
+                string keyName = Console.ReadLine();
+                Console.Write("Enter bin name: ");
+                string binName = Console.ReadLine();
+                Console.Write("Enter value: ");
+                string value = Console.ReadLine();
 
-            Key key = new Key(ns, setName, keyName);
-            Bin bin = new Bin(binName, value);
+                Key key = new Key(ns, setName, keyName);
+                Bin bin = new Bin(binName, value);
 
-            client.Put(policy, key, bin);
-            Console.WriteLine("Record written to DB");
-            LaunchAssistant(client);
+                client.Put(policy, key, bin);
+                Console.WriteLine("Record written to DB");
+                LaunchAssistant(client);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The following error occured while trying to write a record: ");
+                Console.WriteLine(e);
+                LaunchAssistant(client);
+            }
+
         }
 
     }
